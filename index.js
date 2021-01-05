ba3384e236b56d63054cc6daeae01ecdb5eb75a2
Junha <mythpow@gmail.com>
2021-01-05 16:04:45 +0900

    Thuesday

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hi");
+import "core-js";
+import express from "express";
+
+const app = express();
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("Hello from profile");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+app.listen(PORT, handleListening);
