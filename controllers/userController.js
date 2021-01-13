a52ca21c646d4ce089331f36fbcb64e75f7ec5ea
Junha <mythpow@gmail.com>
2021-01-13 15:41:37 +0900

    Wedenessday

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,25 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+import routes from "../routes.js";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  console.log(req.body);
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    res.redirect(routes.home);
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = (req, res) => res.redirect(routes.home);
+
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Logout" });
 export const userDetail = (req, res) =>
